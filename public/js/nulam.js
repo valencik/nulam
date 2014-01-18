571a590a4ecf2a3ea40800a5a3fd2c3180dd1372
Andrew Valencik <mightyhops@gmail.com>
2014-01-17 21:58:53 -0400

    range function in js

---
+++ public/js/nulam.js
@@ -0,0 +1,12 @@
+function range(start, count) {
+    if(arguments.length == 1) {
+        count = start;
+        start = 0;
+    }
+
+    var foo = [];
+    for (var i = 0; i < count; i++) {
+        foo.push(start + i);
+    }
+    return foo;
+}
