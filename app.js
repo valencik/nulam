f5cc4a37ca196577076d6e436664662632334f82
Andrew Valencik <mightyhops@gmail.com>
2014-01-18 11:19:14 -0400

    Cheerio scrapes single hardcoded OEIS page

--- app.js
+++ app.js
@@ -3,10 +3,15 @@
  * Module dependencies.
  */
 
+//Standard requires for express app
 var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+
+//For scraping with Cheerio
+var $ = require('cheerio')
+var request = require('request')
 
 var app = express();
 
@@ -28,6 +33,23 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+
+
+//Cheerio grab for OEIS
+function gotHTML(err, resp, html) {
+  if (err) return console.error(err)
+  var parsedHTML = $.load(html)
+
+  var table710ttnums = parsedHTML('td').filter(function(i, el) {
+    // integer sequence is in child of td(width=710)
+    return $(this).attr('width') === '710';
+  }).children().first().text()
+  console.log("OEIS int sequence: ", table710ttnums);
+}
+
+var domain = 'http://oeis.org/search?q=id:A000040'
+request(domain, gotHTML)
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
