08bbe04061ce53ded179d4582cdfd1e8c7291d71
nate <myles.n.a@gmail.com>
2014-01-17 23:50:50 -0400

    more regexin'

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,28 +1,48 @@
 (function(){
 
-    $(function(){
-        var squares = $('#squares');
+    $(document).ready(function(){
+        squares = $('#squares');
+        input = $('#regex-input');
+        message = $('#message');
 
-        for(var i = 0; i < 100; i++){
+        input.keyup(function(){
+            try {
+                regex = new RegExp($(this).val());
+                runRegex();
+            } catch (e) {
+                message.text('invalid');
+            }
+        });
+
+        for(var i = 0; i < 200; i++){
             data.push(i);
         }
 
         for(var elem in data){
-            squares.append('<div class="square red">' + elem + '</div>');
+            squares.append('<div class="square red">' + (parseInt(elem) + 1) + '</div>');
         }
+    });
 
+    var data = [];
+
+    var squares;
+    var input;
+    var message;
+
+    var regex = /^[1-5]|[2]$/;
+
+    function runRegex(){
+        var matches = 0;
         squares.find('.square').each(function(){
             var square = $(this);
             square.addClass('red').removeClass('green');
             var content = square.text();
             if(regex.test(content)){
                 square.addClass('green').removeClass('red');
+                matches++;
             }
         });
-    });
-
-    var data = [];
-
-    var regex = /^[1-5]|[2]$/;
+        message.text(matches + ' matches');
+    }
 
 }());
