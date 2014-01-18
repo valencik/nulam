5891780b383ac8e3aeb07aa48b5d59a450e3c8dc
nate <myles.n.a@gmail.com>
2014-01-17 23:16:13 -0400

    regexin'

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,15 +1,28 @@
 (function(){
 
     $(function(){
+        var squares = $('#squares');
+
         for(var i = 0; i < 100; i++){
             data.push(i);
         }
 
         for(var elem in data){
-            $('#squares').append('<div class="square red">' + elem + '</div>');
+            squares.append('<div class="square red">' + elem + '</div>');
         }
+
+        squares.find('.square').each(function(){
+            var square = $(this);
+            square.addClass('red').removeClass('green');
+            var content = square.text();
+            if(regex.test(content)){
+                square.addClass('green').removeClass('red');
+            }
+        });
     });
 
     var data = [];
+
+    var regex = /^[1-5]|[2]$/;
 
 }());
